d3f9705c703276598bbed19ea40895dc0e3a9e57
Daniel Dìaz <danieldiazc413@gmail.com>
2023-11-02 10:10:10 -0500

    Login empezado

--- app/pages/LoginView.jsx
+++ app/pages/LoginView.jsx
@@ -1,24 +1,29 @@
 'use client'
 
 import FormLogin from "@/components/FormLogin"
+import { useGlobalState } from "@/hooks/useGlobalState"
 import { Col, Container, Row } from "react-bootstrap"
 
 const LoginView = () => {
 
-
+  const { user } = useGlobalState()
+ 
   return (
     <>
-            <Container>
-              <Row>
-                <Col></Col>
-                <Col className="py-4 d-flex  flex-column justify-content-center align-items-center">
-                  <h1 className="text-white bg-dark p-2 rounded">Inicio de sesión</h1>
-                  <p className="text-muted">Como Cliente o Conductor</p>
-                  <FormLogin/>
-                </Col>
-                <Col></Col>
-              </Row>
-            </Container>
+          {
+            user ? <h1>Estas logueado! felicidades</h1>
+            :  <Container>
+            <Row>
+              <Col></Col>
+              <Col className="py-4 d-flex  flex-column justify-content-center align-items-center">
+                <h1 className="text-white bg-dark p-2 rounded">Inicio de sesión</h1>
+                <p className="text-muted">Como Cliente o Conductor</p>
+                <FormLogin/>
+              </Col>
+              <Col></Col>
+            </Row>
+          </Container>
+          }
     </>
   )
 }
--- components/FormLogin.jsx
+++ components/FormLogin.jsx
@@ -1,40 +1,98 @@
-'use client'
-import { Button, Col, Container, Form, Row } from "react-bootstrap"
+"use client";
+import { useGlobalState } from "@/hooks/useGlobalState"
+import axios from "axios"
+import { MD5 } from "crypto-js"
+import { useState } from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const FormLogin = () => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const email = e.target[0].value
-    const password = e.target[1].value
+  const { user, setUser } = useGlobalState()
 
-    //verify
-    if (email === '') {
-      alert('Ingresa tu correo electrónico')
-    } 
-    if (password === '') {
-      alert('Ingresa tu contraseña')
-    }
+  const [idUsuario, setIdUsuario] = useState(''); 
+  const [codigo2FA, setCodigo2FA] = useState(false);
 
-    //login
-
+  const cifrar = (cadena) => {
+    const cadenaCifrada = MD5(cadena).toString();
+    return cadenaCifrada;
   }
 
-  return (
-    <Form onSubmit={handleSubmit}>
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Ingresa tu correo electrónico" />
-    </Form.Group>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const correo = e.target[0].value;
+    const password = e.target[1].value;
 
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Ingresa tu contraseña" />
-    </Form.Group>
+    const clave = cifrar(password);
+
+    // llamar a seguridad para verificar
+    const response = await axios.post('http://localhost:3001/identificar-usuario' , {
+      correo, 
+      clave
+    })
+
+    setIdUsuario(response.data._id)
+    setCodigo2FA(true)
     
-    <Button variant="primary" type="submit">
-        Iniciar sesión
-    </Button>
-    </Form>
-  )
-}
+  };
 
-export default FormLogin
+  const handleSubmit2FA = async (e) => {
+    e.preventDefault();
+    const codigo = e.target[0].value;
+
+    // llamar a seguridad para verificar codigo 2fa
+    const response = await axios.post('http://localhost:3001/verificar-2fa', {
+      codigo2fa: codigo,
+      usuarioId: idUsuario
+    })
+
+    console.log(response.data)
+
+    const token = response.data.token
+    localStorage.setItem('token', token)
+
+    setUser(response.data)
+
+  };
+
+  return (
+    <>
+      {!codigo2FA ? (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="email"
+              placeholder="Ingresa tu correo electrónico"
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Control type="password" placeholder="Ingresa tu contraseña" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Iniciar sesión
+          </Button>
+        </Form>
+      ) : (
+        <Form onSubmit={handleSubmit2FA}>
+          <h3 className="py-2">Autenticación de 2do Factor</h3>
+          <p className="text-muted">
+            Te enviamos un código, revisa tu correo.
+          </p>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="text"
+              placeholder="Ingresa el código"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Verificar
+          </Button>
+        </Form>
+      )}
+    </>
+  );
+};
+
+export default FormLogin;
